fce864b5aeebac51fd18c73902e818b5c56c336a
JJ <JJ>
2015-07-29 18:09:17 +0800

    modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,6 @@ Cari::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   match ':controller(/:action(/:id))(.:format)'
+  match '*not_found', :to => 'error#not_found'
+  
 end
